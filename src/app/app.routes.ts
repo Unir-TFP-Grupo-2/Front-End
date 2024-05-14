7cb5f94b6767ae2d6c2997183aaf116d6e085e8c
VictorLM98 <viictorluquem@gmail.com>
2024-05-14 21:43:57 +0200

    Creación Rutas
    groups, account, about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GroupsComponent } from './pages/groups/groups.component';
+import { AccountComponent } from './pages/account/account.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", pathMatch: "full", redirectTo: "welcome"},
+    {path: "welcome", component: WelcomeComponent },
+    {path: "login", component: LoginComponent},
+    {path: "register", component: RegisterComponent },
+    {path: "home", component: HomeComponent},
+    {path: "groups", component: GroupsComponent},
+    {path: "account", component: AccountComponent},
+    {path: "about", component: AboutComponent}
+];
