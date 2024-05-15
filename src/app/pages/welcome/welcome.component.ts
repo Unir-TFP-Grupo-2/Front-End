f14c1fcfc763bf7430943f0bc247f4feb3f7706c
victor <vgomez@emfasi.com>
2024-05-15 15:10:44 +0200

    componentes caracteristicas

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HomeComponentSliderComponent } from '../../home-component-slider/home-component-slider.component';
+import { HomeComponentGrid2Component } from '../../home-component-grid2/home-component-grid2.component';
+import { HomeComponentCaracteristicasComponent } from '../../home-component-caracteristicas/home-component-caracteristicas.component';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [HomeComponentSliderComponent],
+  imports: [HomeComponentSliderComponent, HomeComponentGrid2Component, HomeComponentCaracteristicasComponent],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
