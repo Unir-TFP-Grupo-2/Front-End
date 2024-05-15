46676feea99d950b91ca71d2328da23fb59197d7
victor <vgomez@emfasi.com>
2024-05-15 15:33:01 +0200

    Como funciona

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HomeComponentSliderComponent } from '../../home-component-slider/home-component-slider.component';
 import { HomeComponentGrid2Component } from '../../home-component-grid2/home-component-grid2.component';
 import { HomeComponentCaracteristicasComponent } from '../../home-component-caracteristicas/home-component-caracteristicas.component';
+import { HomeComponentFuncionComponent } from '../../home-component-funcion/home-component-funcion.component';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [HomeComponentSliderComponent, HomeComponentGrid2Component, HomeComponentCaracteristicasComponent],
+  imports: [HomeComponentSliderComponent, HomeComponentGrid2Component, HomeComponentCaracteristicasComponent, HomeComponentFuncionComponent],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
