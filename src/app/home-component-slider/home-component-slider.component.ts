f080d1132986949d563bd17042502e81f46fdfc6
victor <vgomez@emfasi.com>
2024-05-15 08:44:09 +0200

    footer y header

---
+++ src/app/home-component-slider/home-component-slider.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-component-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-component-slider.component.html',
+  styleUrl: './home-component-slider.component.css'
+})
+export class HomeComponentSliderComponent {
+
+}
--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HomeComponentSliderComponent } from '../../home-component-slider/home-component-slider.component';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [HomeComponentSliderComponent],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
