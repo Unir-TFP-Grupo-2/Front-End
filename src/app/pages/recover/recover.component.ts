5966a5c781c89b6c832102c43d71c09aa15b67a7
VictorLM98 <viictorluquem@gmail.com>
2024-05-16 18:57:37 +0200

    Log in / register / Recover / Maquetacion

---
+++ src/app/pages/recover/recover.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-recover',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './recover.component.html',
+  styleUrl: './recover.component.css'
+})
+export class RecoverComponent {
+
+}
