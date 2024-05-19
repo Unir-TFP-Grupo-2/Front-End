0e76ffff23c7344d1b3efd8b7e618eaafd94279c
victor <vgomez@emfasi.com>
2024-05-19 20:13:06 +0200

    borrar logica

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -4,12 +4,12 @@ import { HomeComponentGrid2Component } from '../../components/home-component-gri
 import { HomeComponentCaracteristicasComponent } from '../../components/home-component-caracteristicas/home-component-caracteristicas.component';
 import { HomeComponentFuncionComponent } from '../../components/home-component-funcion/home-component-funcion.component';
 import { ComponentFaqsComponent } from '../../components/component-faqs/component-faqs.component';
-import { LogicaComponent } from '../../components/logica/logica.component';
+
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [HomeComponentSliderComponent, HomeComponentGrid2Component, HomeComponentCaracteristicasComponent, HomeComponentFuncionComponent, ComponentFaqsComponent,LogicaComponent],
+  imports: [HomeComponentSliderComponent, HomeComponentGrid2Component, HomeComponentCaracteristicasComponent, HomeComponentFuncionComponent, ComponentFaqsComponent],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
