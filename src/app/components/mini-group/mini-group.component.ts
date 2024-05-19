bed5107d88b47c903ae7b5c4790f27b23972131c
Cristian Requena <cristian91rp@gmail.com>
2024-05-19 19:49:46 +0200

    Creación component/mini-group

---
+++ src/app/components/mini-group/mini-group.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-mini-group',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './mini-group.component.html',
+  styleUrl: './mini-group.component.css'
+})
+export class MiniGroupComponent {
+
+}
