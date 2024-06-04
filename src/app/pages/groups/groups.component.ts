51ad24fa69d1fff408fc912e1b3cbcefdf93a175
Cristian Requena <cristian91rp@gmail.com>
2024-06-04 20:14:04 +0200

    Mockup de Grupos

--- src/app/pages/groups/groups.component.ts
+++ src/app/pages/groups/groups.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-groups',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule],
   templateUrl: './groups.component.html',
   styleUrl: './groups.component.css'
 })
