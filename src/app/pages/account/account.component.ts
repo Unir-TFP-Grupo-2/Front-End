fd034f0d198d66356fdbc12ccfcb04573c14190e
Pedro José Fernández Merlo <pedrojosefm.91@gmail.com>
2024-06-12 11:55:13 +0200

    he añadido input y cambiar contraseña y imagen

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
@@ -15,12 +16,65 @@ export class AccountComponent {
   name = 'PEDRO';
   lastname = 'FERNÁNDEZ';
   email = 'PEDRO@GMAIL.COM';
-  photo = 'https://i.pravatar.cc/500?u=mariadelcarmen.herreravillanueva@peticiones.online';
   fechaNacimiento = '22/02/1990';
   password = '2564564';
   passwordVisible = false;
   Phone = '652874555';
-  
+  photo: string | undefined;
+
+
+
+cambiarFoto(event: any) {
+  if (event.target.files && event.target.files[0]) {
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.photo = e.target.result;
+    };
+    reader.readAsDataURL(event.target.files[0]);
+  }
 }
+
+private http: HttpClient;
+
+constructor(http: HttpClient) {
+  this.http = http;
+  this.passwordActual = 'contraseñaActual';
+}
+
+guardarCambios() {
+
+  const userDetails = {
+    Username: this.Username,
+    name: this.name,
+    lastname: this.lastname,
+    Phone: this.Phone,
+    email: this.email
+  };
+
+  this.http.put('', userDetails)
+  .subscribe(response => {
+    console.log('Cambios guardados:', response);
+  }, error => {
+    console.error('Error al guardar los cambios:', error);
+  });
+}
+
+cambiarpassword = false;
+passwordActual: string;
+
+cambiarPassword(nuevaContraseña: string) {
+  const passwordDetails = {
+    password: nuevaContraseña
+  };
+
+  this.http.put('https://mi-api.com/usuarios/123/password', passwordDetails)
+    .subscribe(response => {
+      console.log('Contraseña actualizada:', response);
+    }, error => {
+      console.error('Error al actualizar la contraseña:', error);
+    });
+}
+}
+
 
 
