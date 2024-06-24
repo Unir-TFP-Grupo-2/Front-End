48e2783ed1ecc9b688adc6d91a86d5ea3761cacb
victor <vgomez@emfasi.com>
2024-06-24 11:52:01 +0200

    modificar account ts

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -18,11 +18,11 @@ export class AccountComponent {
   password = '';
   newPasswordVisible = false;
   passwordVisible = false;
-  photo: string | undefined;
+  photo: Blob | undefined;
   usuarios: any[] = [];
   id: string | null = null;
 
-  
+
   activatedRouter = inject(ActivatedRoute);
   baseUrl: string;
   httpClient: HttpClient;
@@ -30,7 +30,7 @@ export class AccountComponent {
   nuevaPassword: any;
   newPassword: any;
   userId: any;
-  
+
 
 
   constructor(
@@ -47,7 +47,8 @@ export class AccountComponent {
   ngOnInit() {
     this.id = this.activatedRoute.snapshot.paramMap.get('id');
     this.activatedRoute.params.subscribe(async (params: any) => {
-      this.obtenerUsuarios(params.id);
+      this.userId = localStorage.getItem('user_id');
+      this.obtenerUsuarios(this.userId);
     }
     );
     console.log(localStorage.getItem('token_usuario'));
@@ -55,21 +56,20 @@ export class AccountComponent {
   }
 
   obtenerUsuarios(id: number) {
+
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': 'Bearer ' + localStorage.getItem('token_usuario')
     });
-    console.log(localStorage.getItem('token_usuario'));
-    const userId = localStorage.getItem('user_id');
-    this.http.get(`http://localhost:3000/api/usuarios/${userId}`, { headers }).subscribe({
+    this.http.get(`http://localhost:3000/api/usuarios/${id}`, { headers }).subscribe({
       next: (response: any) => {
-        console.log("Respuesta del servidor:", response); 
+        console.log("Respuesta del servidor:", response);
         this.name = response.name;
         this.lastname = response.lastname;
         this.email = response.email;
         this.photo = response.photo;
         this.password = response.password;
-        this.usuarios = [response]; 
+        this.usuarios = [response];
         console.log("Usuarios:", this.usuarios);
       },
       error: (error) => {
@@ -77,40 +77,40 @@ export class AccountComponent {
       }
     });
   }
-    cambiarFoto(event: any) {
-      if (event.target.files && event.target.files[0]) {
-        const reader = new FileReader();
-        reader.onload = (e: any) => {
-          this.photo = e.target.result;
-        };
-        reader.readAsDataURL(event.target.files[0]);
-      }
-    }
-    async guardarCambios() {
-      const userId = localStorage.getItem('user_id');
-      if (!userId) {
-        console.error('El ID del usuario no está definido');
-        return;
-      }
-    
-      const userDetailsActualizado: any = {
-        name: this.name,
-        lastname: this.lastname,
-        email: this.email,
-        photo: this.photo,
-        password: this.password
+  cambiarFoto(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.photo = e.target.result;
       };
-    
-      if (this.cambiarpassword && this.newPassword) {
-        userDetailsActualizado.password = this.newPassword;
-      }
-    
-      try { 
-        await this.usersService.updateUser(userId, userDetailsActualizado); 
-        await this.router.navigate([`/account`]); 
-        console.log('Cambios guardados con éxito'); 
-      } catch (error) {  
-        console.error('Error al guardar los cambios:', error); 
-      } 
+      reader.readAsDataURL(event.target.files[0]);
     }
-    } 
+  }
+  async guardarCambios() {
+    const userId = localStorage.getItem('user_id');
+    if (!userId) {
+      console.error('El ID del usuario no está definido');
+      return;
+    }
+
+    const userDetailsActualizado: any = {
+      name: this.name,
+      lastname: this.lastname,
+      email: this.email,
+      photo: this.photo,
+      password: this.password
+    };
+
+    if (this.cambiarpassword && this.newPassword) {
+      userDetailsActualizado.password = this.newPassword;
+    }
+
+    try {
+      await this.usersService.updateUser(userId, userDetailsActualizado);
+      await this.router.navigate([`/account`]);
+      console.log('Cambios guardados con éxito');
+    } catch (error) {
+      console.error('Error al guardar los cambios:', error);
+    }
+  }
+} 
